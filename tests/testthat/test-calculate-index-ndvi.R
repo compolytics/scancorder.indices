63e1a74a2b80b705e4baf76e65c2bc7318a68050
Andreas Backhaus <andreas.backhaus@compolytics.com>
2025-04-24 10:04:39 +0200

    more tests

---
+++ tests/testthat/test-calculate-index-ndvi.R
@@ -0,0 +1,19 @@
+test_that("test calculating NDVI from CICADA json with single point reflectance and sensor average",
+          {
+            # What to expect, a list of NDVI
+            expectedNDVI = list(0.27145945207973060098)
+
+            json_path <- testthat::test_path(
+              "data/20250303_150545_SA1211_B3009_S5221_08445c21-184a-4082-8597-e11f62e3561e_0001.json"
+            )
+            # Load the JSON file with the sensor reading
+            json_input <- paste(readLines(json_path), collapse = "\n")
+            # Setup a decoder that will average the reflectance per LED across sensor channels
+            decoder <- DecodeCompolyticsRegularScanner$new(average_sensor_values = TRUE)
+            # Decode the JSON input to get the reflectance values
+            data <- decoder$score(json_input)
+            # Calculate Index
+            ndvi_value <- calculate_index('ndvi.xml', data$wavelengths, data$reflectance)
+            # Check if the NDVI value is as expected
+            expect_equal(data$reflectance, expectedNDVI)
+          })
